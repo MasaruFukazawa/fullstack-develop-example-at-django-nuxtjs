932b7b3bbd8b58ffabb6df0c289abc0353182b85
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-05-07 10:16:37 +0900

    ref、onMountedを使用して状態変更させてみた

---
+++ frontend/fullstack-develop-example-at-django-nuxtjs/pages/hello_frontend_ref.vue
@@ -0,0 +1,15 @@
+/**
+ * 一度、「初期値」を描画後に「変更」に変更する
+ */ 
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const word = ref('初期値')
+
+onMounted(() => {
+    word.value = '変更'
+})
+</script>
+<template>
+    <h1>Hello, {{ word }}</h1>
+</template>
