25965c49c74c97a583f8fcc50ec4d80ee8796fed
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-05-09 03:14:49 +0900

    モデルありのAPIを受け取るVuePageを作成

---
+++ frontend/fullstack-develop-example-at-django-nuxtjs/pages/hello_backend_db.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+const {data: word} = await useFetch('http://host.docker.internal:8000/api/hello_db/backend')
+
+</script>
+<template>
+    <h1>Hello, {{ word.message }}</h1>
+</template>
