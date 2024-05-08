ed3e774e264776ed41076b612d71dee15c6c37b8
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-05-08 10:25:13 +0900

    バックエンドとAPI連携してみた

--- frontend/fullstack-develop-example-at-django-nuxtjs/pages/hello_backend.vue
+++ frontend/fullstack-develop-example-at-django-nuxtjs/pages/hello_backend.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
+const ver = ref(1)
 const {data: word} = await useFetch('http://host.docker.internal:8000/api/hello/backend')
 
 </script>
 <template>
-    <h1>Hello, {{ word.message }}</h1>
+    <h1>Hello, {{ word.message }} ver{{ ver }}</h1>
 </template>
