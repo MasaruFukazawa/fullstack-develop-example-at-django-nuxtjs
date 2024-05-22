d6ef3ea3ef812dde42bb9f14fd9c880fb2b1455c
Masaru Fukazawa <upw.ponsuke@gmail.com>
2024-05-23 00:15:43 +0900

    商品一覧実装

---
+++ frontend/fullstack-develop-example-at-django-nuxtjs/pages/products/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { data: products, pending, error, refresh } = await useFetch('http://host.docker.internal:8000/api/inventory/products/')
+
+</script>
+
+<template>
+    <UContainer>
+        <h2>商品一覧</h2>
+        <UTable :rows="products" />
+    </UContainer>
+</template>
